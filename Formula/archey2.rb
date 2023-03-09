a8768a69bc3be9752de3c7e411d2711082564da5
Jacob Pszonowsky <jake@findjake.com>
2023-03-09 07:57:36 -0800

    updated for v1.0.4

--- Formula/archey2.rb
+++ Formula/archey2.rb
@@ -2,13 +2,14 @@ class Archey2 < Formula
   desc "Updated archey graphical system information display for macOS"
   homepage "https://jakepez.github.io/archey2-osx/"
   license "GPL-2.0-or-later"
-  version "1.0.4"
+  url "https://github.com/jakepez/archey2-osx/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "dc04d007822dfbf9d60068f0a1c734e3a5e27fef2ebed7686192e26b2e55852c"
   head "https://github.com/jakepez/archey2-osx.git", branch: "main"
 
-  stable do
-    url "https://github.com/jakepez/archey2-osx/archive/refs/tags/v1.0.4.tar.gz"
-    sha256 "dc04d007822dfbf9d60068f0a1c734e3a5e27fef2ebed7686192e26b2e55852c"
-  end
+  #stable do
+  #  url "https://github.com/jakepez/archey2-osx/archive/refs/tags/v1.0.4.tar.gz"
+  #  sha256 "dc04d007822dfbf9d60068f0a1c734e3a5e27fef2ebed7686192e26b2e55852c"
+  #end
 
   # Optionally, specify a repository to be used. Brew then generates a
   # `--HEAD` option. Remember to also test it.
